ac8344eebb021d7bbc260b5fa46b5c78950a4392
anna <annalopatkina11@gmail.com>
2021-01-27 20:13:07 +0200

    Step2

---
+++ Makefile
@@ -0,0 +1,6 @@
+install:
+	npm install
+lint:
+	npx eslint .
+gendiff:
+	 npm link
---
+++ src/bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+
+  program.parse(process.argv);
