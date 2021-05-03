443692a622e028c13d84f11a5cb3e43b520d3c52
anna <annalopatkina11@gmail.com>
2021-05-03 20:57:34 +0300

    Step4Fix3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,6 +3,6 @@ import { test, expect } from '@jest/globals';
 import result from '../__fixtures__/result';
 
 
-test('diffOfJson', () => {
-    expect(genDiff().toEqual(result));
-})
+test('genDiff', () => {
+    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
+});
