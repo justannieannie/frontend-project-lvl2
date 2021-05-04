9e086a4f1154da3d38319a914a1c313c8385ebb5
anna <annalopatkina11@gmail.com>
2021-05-04 22:41:03 +0300

    FixLinter

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 install:
 	npm install
 
-lint:
-	npx eslint .
-	
 gendiff:
 	npm link
 
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,7 @@
-import genDiff from '../src/index'
 import { test, expect } from '@jest/globals';
+import genDiff from '../src/index';
 import result from '../__fixtures__/result';
 
-
 test('genDiff', () => {
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
 });
