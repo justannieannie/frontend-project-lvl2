90d4632e285330506ec6602350b780a4ba5d914c
anna <annalopatkina11@gmail.com>
2021-05-07 23:26:46 +0300

    Step5LinterErrorsFix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,11 +3,9 @@ import genDiff from '../src/index';
 import result from '../__fixtures__/result';
 
 test('genDiff.json', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).
-  toEqual(result);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(result);
 });
 
 test('genDiff.yml', () => {
-  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).
-  toEqual(result);
+  expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(result);
 });
