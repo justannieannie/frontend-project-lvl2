23a0057d30b8ca16939febd0396395b0d84ae562
anna <annalopatkina11@gmail.com>
2021-06-15 00:12:54 +0200

    LinterFix2

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -3,7 +3,8 @@ import _ from 'lodash';
 const makeDiff = (obj1, obj2) => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const result = keys.map((key) => {
-    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key]) && !_.isEqual(obj1[key], obj2[key])) {
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])
+    && !_.isEqual(obj1[key], obj2[key])) {
       return { key, children: makeDiff(obj1[key], obj2[key]), type: 'nested' };
     }
     if (!_.has(obj1, key)) {
